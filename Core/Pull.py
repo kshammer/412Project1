150ff78ee0519dc87260f0135a6e1df7594db513
kshammer <hammerschmiks@vcu.edu>
2017-10-22 13:11:14 -0400

    Added list of players

--- Core/Pull.py
+++ Core/Pull.py
@@ -1,17 +1,30 @@
 import requests, time
 import numpy as np
+
+
+def addToPlayers(matchPlayers):
+    for p in matchPlayers:
+        if p is not None and p in players:
+            for z in matchPlayers:
+                if z is not None:
+                    players[p].append(z)
+        elif p is not None:
+            players[p] = []
+            for z in matchPlayers:
+                if z is not None:
+                    players[p].append(z)
 #tims 114958061
 #kyles 83615933 random match 3506289918
 #lobby 1873152
 #amin 130643254
 #bradon 76854778
-steamid = "130643254"
+steamid = "83615933"
 amountMatches = 10
 #limit is how many
 #offset is by how many matches back to start
 mmrAverages = []
-players = [[]]
-for z in range(10):
+players = {}
+for z in range(3):
     print(z)
     matchIds = []
     mmrs = []
@@ -22,25 +35,20 @@ for z in range(10):
     time.sleep(1)
     for i in range(amountMatches):
         q = requests.get("https://api.opendota.com/api/matches/" + str(matchIds[i]))
-
+        matchPlayers = []
         for p in range(10):
             try:
                 print(q.json()['players'][p]['account_id'])
-                if q.json()['players'][p]['account_id'] is not None and q.json()['players'][p]['account_id'] not in players:
-
+                matchPlayers.append(q.json()['players'][p]['account_id'])
                 if q.json()['players'][p]['solo_competitive_rank'] is not None and (q.json()['players'][9]['party_id'] == 9 or q.json()['players'][9]['party_id'] is None):
                     #print(q.json()['players'][p]['solo_competitive_rank'])
                     mmrs.append(q.json()['players'][p]['solo_competitive_rank'])
             except Exception:
                 print("Could not parse / caught error")
         time.sleep(1)
+        addToPlayers(matchPlayers)
     print(mmrs)
     print(np.mean(mmrs))
     mmrAverages.append(np.mean(mmrs))
 print(mmrAverages)
-
-
-
-
-
-
+print(players)
