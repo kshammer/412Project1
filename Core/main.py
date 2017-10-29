1288ca339f1b31667f0626d061b5b31dd26a227b
kshammer <hammerschmiks@vcu.edu>
2017-10-29 15:33:04 -0400

    now have working verision of main.

--- Core/main.py
+++ Core/main.py
@@ -1,9 +1,15 @@
 from Core.dotadata import dotadata
+import csv
 
 steamid = "83615933"
-test = dotadata(steamid)
+test = dotadata(steamid, batches=5,batchsize=5)
 print("Starting")
 test.getData()
 print("done and printing out the players dictionary")
-for k,v in test.players.items():
-    print(k, v)
+with open("mmrs.csv", 'w') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerow(test.mmrAverages)
+with open('players.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file)
+    for key, value in test.players.items():
+        writer.writerow([key, value])
--- Core/test.py
+++ Core/test.py
@@ -1,1 +1,8 @@
-print("<Response [200]>" == "<Response [200]>")
+import csv
+
+my_dict = {"test": 1, "testing": 2}
+
+with open('mycsvfile.csv', 'w') as w:  # Just use 'w' mode in 3.x
+    w = csv.DictWriter(w, my_dict.keys())
+    w.writeheader()
+    w.writerow(my_dict)
