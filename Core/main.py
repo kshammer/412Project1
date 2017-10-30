cdd202e4e2c646b9d953c66e12364b51d152d489
kshammer <hammerschmiks@vcu.edu>
2017-10-30 10:59:27 -0400

    Fixed output for gephi

--- Core/main.py
+++ Core/main.py
@@ -5,11 +5,15 @@ steamid = "83615933"
 test = dotadata(steamid, batches=5,batchsize=5)
 print("Starting")
 test.getData()
+
 print("done and printing out the players dictionary")
-with open("mmrs.csv", 'w') as myfile:
-    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-    wr.writerow(test.mmrAverages)
-with open('players.csv', 'w') as csv_file:
+
+with open("nodes.csv", 'w') as myfile:
+    wr = csv.writer(myfile)
+    for z in test.mmrAverages:
+        wr.writerow([z])
+with open('edgelist.csv', 'w') as csv_file:
     writer = csv.writer(csv_file)
     for key, value in test.players.items():
-        writer.writerow([key, value])
+        for per in value:
+            writer.writerow(([key], [per]))
--- Core/test.py
+++ Core/test.py
@@ -1,8 +1,21 @@
 import csv
 
-my_dict = {"test": 1, "testing": 2}
+my_dict = {5: [1, 2, 3], "testing": [2]}
 
-with open('mycsvfile.csv', 'w') as w:  # Just use 'w' mode in 3.x
-    w = csv.DictWriter(w, my_dict.keys())
-    w.writeheader()
-    w.writerow(my_dict)
+cool = list(my_dict.keys())
+print(cool)
+for k, v in my_dict.items():
+    for l in v:
+        print(k, l)
+
+# for node list
+with open('mycsvfile.csv', 'w', newline="") as w:  # Just use 'w' mode in 3.x
+    writer = csv.writer(w)
+    for z in cool:
+        writer.writerow([z])
+#for edge list
+with open('edgelisasdfasdt.csv', 'w', newline="") as q:
+    write = csv.writer(q)
+    for i,e in my_dict.items():
+        for y in e:
+            write.writerow(([i], [y]))
