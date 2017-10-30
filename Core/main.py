85afd86446f3a203215bf4282c1cd47151b68c34
kshammer <hammerschmiks@vcu.edu>
2017-10-30 13:15:07 -0400

    added edge lists for gephi and updated main

--- Core/main.py
+++ Core/main.py
@@ -1,6 +1,6 @@
 from Core.dotadata import dotadata
 import csv
-
+import numpy as np
 steamid = "83615933"
 test = dotadata(steamid, batches=5,batchsize=5)
 print("Starting")
@@ -8,12 +8,14 @@ test.getData()
 
 print("done and printing out the players dictionary")
 
+cool = list(test.players.keys())
 with open("nodes.csv", 'w', newline="") as myfile:
     wr = csv.writer(myfile)
-    for z in test.mmrAverages:
+    for z in cool:
         wr.writerow([z])
 with open('edgelist.csv', 'w', newline="") as csv_file:
     writer = csv.writer(csv_file)
     for key, value in test.players.items():
         for per in value:
             writer.writerow(([key], [per]))
+print(np.mean(test.mmrAverages))
